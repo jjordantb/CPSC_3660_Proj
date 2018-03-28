0c02b134c2800f0fa4988ef27281c9355bd39c40
jjordantb <jordan.florchinger@uleth.ca>
2018-03-28 13:44:40 -0600

    basic files

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <link rel="stylesheet" type="text/css" href="main.css">
+</head>
+
+<body>
+    <header>
+        Our Page
+    </header>
+</body>
